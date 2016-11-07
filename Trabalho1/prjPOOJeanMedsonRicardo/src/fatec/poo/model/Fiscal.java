506ee6066f0cfb9362ecb262e4971ba9eabb36a0
rhiroyuki <ricardo.hiroyuki@outlook.com>
2016-11-07 16:04:39 -0200

    Fix fiscal class

--- Trabalho1/prjPOOJeanMedsonRicardo/src/fatec/poo/model/Fiscal.java
+++ Trabalho1/prjPOOJeanMedsonRicardo/src/fatec/poo/model/Fiscal.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package fatec.poo.model;
 
 /**
@@ -14,16 +8,13 @@ public class Fiscal extends Pessoa{
     private String codigo;
     private String local;
 
-    public Fiscal(String cpf, String nome, String endereco) {
+    public Fiscal(String codigo, String cpf, String nome, String endereco) {
         super(cpf, nome, endereco);
-    } 
+        this.codigo = codigo;
+    }
 
     public String getCodigo() {
         return codigo;
-    }
-
-    public void setCodigo(String codigo) {
-        this.codigo = codigo;
     }
 
     public void setLocal(String local) {
